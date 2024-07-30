ad2f9b62a1e08320c0c0515d3e900310229e35f3
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-07-30 20:15:45 -0300

    [FEAT] Products section

--- page-home.php
+++ page-home.php
@@ -1,14 +1,21 @@
 <?php get_header(); ?>
+<?php $siteUrl = site_url(); ?>
 
 <?php
-$args = array(
-    'taxonomy'   => 'product_cat',
-    'orderby'    => 'name',
-    'order'      => 'ASC',
-    'exclude' => array(15),
-    'hide_empty' => false
-);
-$product_categories = get_terms($args);
+function getProductCategories($categoryIds=[]){
+    $categoriesToInclude = $categoryIds ? $categoryIds : "";
+    $productCatArgs = array(
+        'taxonomy'   => 'product_cat',
+        'orderby'    => 'name',
+        'order'      => 'ASC',
+        'include'   =>  $categoriesToInclude,
+        'exclude' => array(15),
+        'hide_empty' => false
+    );
+    $productCategories = get_terms($productCatArgs);
+    
+    return $productCategories;
+}
 
 ?>
 
@@ -47,14 +54,16 @@ if($carousel){ ?>
     </section>
 <?php } ?>
 
-<section id="section_products">
+<section id="section_categories">
     <div class="container">
         <div class="row">
-            <?php foreach($product_categories as $cat){ ?>
+            <?php 
+            $productCat = getProductCategories();
+            foreach($productCat as $cat){ ?>
                 <?php $thumbnailId = get_term_meta( $cat->term_id, 'thumbnail_id', true ); ?>
                 <?php $thumbnailUrl = wp_get_attachment_url( $thumbnailId ); ?>
 
-                    <a href="/categoria-produto/<?php echo $cat->slug; ?>" class="category__card col-md-6">
+                    <a href=<?php echo "$siteUrl/categoria-produto/$cat->slug";?> class="category__card col-md-6">
                         <div style="background: url(<?php echo esc_url( $thumbnailUrl ); ?>)" class="category__card_img col-md-6"></div>
 
                         <div class="category__card_info col-md-6 d-flex flex-column justify-content-center">
@@ -77,9 +86,9 @@ if($carousel){ ?>
             <div class="row align-items-center justify-content-between">
                 <div class="col-md-4">
                     <div class="about__info">
-                        <h3 class="about__title">
+                        <h2 class="section__title">
                             <?php echo $aboutSection["title"]; ?>
-                        </h3>
+                        </h2>
     
                         <p class="about__description">
                             <?php echo $aboutSection["description"]; ?>
@@ -109,6 +118,53 @@ if($carousel){ ?>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/j7UMEFhfwcU?si=eIxtDAa-zuwGrqwx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen="allowfullscreen"></iframe>
     </div>
 <?php } ?>
+
+
+
+<section id="section_products">
+    <div class="container">
+        <h2 class="section__title text-center">Destaques</h2>
+        <div class="row">
+            <?php 
+            $getProductsArgs = array(
+                'limit' => 8,
+                'status' => 'publish'
+            );
+            $products = wc_get_products( $getProductsArgs );
+
+            if($products){
+                foreach($products as $product){  
+                $productImage = get_the_post_thumbnail($product->id, 'full');
+                ?>
+                    <div class="col-md-3 mb-5">
+                            <div class="product__card">
+                                <a class="product__img" href=<?php echo "$siteUrl/produto/$product->slug";?>>
+                                    <?php echo $productImage; ?>
+                                    <div class="product__card_img_btn">
+                                        <span>Veja mais</span>
+                                        <i class="fa-solid fa-arrow-right-long"></i>
+                                    </div>
+                                </a>
+
+                                <div class="product__card_info d-flex flex-column text-center">
+                                    <a href=<?php echo "$siteUrl/produto/$product->slug";?> class="product__title">
+                                        <?php echo $product->name; ?>
+                                    </a>
+    
+                                    <?php foreach(getProductCategories($product->category_ids) as $productCat){ ?>
+                                        <a href=<?php echo "$siteUrl/categoria-produto/$cat->slug";?> class="product__category">
+                                            <?php echo $productCat->name; ?>
+                                        </a>
+                                   <?php } ?>
+                                </div>
+                            </div>
+                    </div>
+                <?php }
+            }
+            ?>
+        </div>
+    </div>
+</section>
 
 <script>
     //SWIPER SLIDE
