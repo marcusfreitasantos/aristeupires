cb4cfde8e536786440777e706f95918e1e12af2a
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-08-03 18:18:40 -0300

    [FEAT] Logic to query stores based on state

--- page-onde-encontrar.php
+++ page-onde-encontrar.php
@@ -2,34 +2,70 @@
 
 <?php
 $brazilianStates = [
-    'AC - Acre',
-    'AL - Alagoas',
-    'AP - Amapá',
-    'AM - Amazonas',
-    'BA - Bahia',
-    'CE - Ceará',
-    'DF - Distrito Federal',
-    'ES - Espírito Santo',
-    'GO - Goiás',
-    'MA - Maranhão',
-    'MT - Mato Grosso',
-    'MS - Mato Grosso do Sul',
-    'MG - Minas Gerais',
-    'PA - Pará',
-    'PB - Paraíba',
-    'PR - Paraná',
-    'PE - Pernambuco',
-    'PI - Piauí',
-    'RJ - Rio de Janeiro',
-    'RN - Rio Grande do Norte',
-    'RS - Rio Grande do Sul',
-    'RO - Rondônia',
-    'RR - Roraima',
-    'SC - Santa Catarina',
-    'SP - São Paulo',
-    'SE - Sergipe',
-    'TO - Tocantins'
-]
+    (object) ['name' => 'Acre', 'value' => 'AC'],
+    (object) ['name' => 'Alagoas', 'value' => 'AL'],
+    (object) ['name' => 'Amapá', 'value' => 'AP'],
+    (object) ['name' => 'Amazonas', 'value' => 'AM'],
+    (object) ['name' => 'Bahia', 'value' => 'BA'],
+    (object) ['name' => 'Ceará', 'value' => 'CE'],
+    (object) ['name' => 'Distrito Federal', 'value' => 'DF'],
+    (object) ['name' => 'Espírito Santo', 'value' => 'ES'],
+    (object) ['name' => 'Goiás', 'value' => 'GO'],
+    (object) ['name' => 'Maranhão', 'value' => 'MA'],
+    (object) ['name' => 'Mato Grosso', 'value' => 'MT'],
+    (object) ['name' => 'Mato Grosso do Sul', 'value' => 'MS'],
+    (object) ['name' => 'Minas Gerais', 'value' => 'MG'],
+    (object) ['name' => 'Pará', 'value' => 'PA'],
+    (object) ['name' => 'Paraíba', 'value' => 'PB'],
+    (object) ['name' => 'Paraná', 'value' => 'PR'],
+    (object) ['name' => 'Pernambuco', 'value' => 'PE'],
+    (object) ['name' => 'Piauí', 'value' => 'PI'],
+    (object) ['name' => 'Rio de Janeiro', 'value' => 'RJ'],
+    (object) ['name' => 'Rio Grande do Norte', 'value' => 'RN'],
+    (object) ['name' => 'Rio Grande do Sul', 'value' => 'RS'],
+    (object) ['name' => 'Rondônia', 'value' => 'RO'],
+    (object) ['name' => 'Roraima', 'value' => 'RR'],
+    (object) ['name' => 'Santa Catarina', 'value' => 'SC'],
+    (object) ['name' => 'São Paulo', 'value' => 'SP'],
+    (object) ['name' => 'Sergipe', 'value' => 'SE'],
+    (object) ['name' => 'Tocantins', 'value' => 'TO'],
+];
+?>
+
+<?php
+$storeStateToSearch = "";
+if(isset($_GET['store_state'])){
+    $storeStateToSearch = $_GET['store_state'];
+}
+
+    $getPostsArgs = array(
+		'posts_per_page'      => 8,
+		'orderby'          => 'date',
+		'order'            => 'DESC',
+		'post_type'        => 'loja',
+        'meta_query' => array(
+            array(
+                'key'     => 'state', 
+                'value'   => $storeStateToSearch, 
+                'compare' => '=',
+            ),
+        ),
+	);	
+    
+    $stores = new WP_Query($getPostsArgs);
+
+
+if ($stores->have_posts()) {
+    while ($stores->have_posts()) {
+        $stores->the_post();
+        the_title('<h2>', '</h2>');
+        the_content();
+    }
+    wp_reset_postdata();
+} else{
+    echo "Não encontrado.";
+}
+
 ?>
 
 
@@ -45,7 +81,7 @@ $brazilianStates = [
                         <option value="">Selecione um estado</option>
 
                         <?php foreach($brazilianStates as $state){ ?>
-                            <option value=<?php echo $state; ?>><?php echo $state; ?></option>
+                            <option value=<?php echo $state->value; ?>><?php echo $state->name; ?></option>
                        <?php } ?>
                     </select>
                 </div>
