8944ccd8c516122deb40e4710807679d90d63c32
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-08-03 11:18:00 -0300

    [FEAT] Section location

--- components/custom-footer/custom-footer.php
+++ components/custom-footer/custom-footer.php
@@ -8,10 +8,113 @@
     </div>
 </section>
 
+<section id="section_location">
+	<div class="container">
+		<h2 class="section__title">Onde estamos</h2>
+		<hr/>
+
+		<!-- BRAZIL LOCATION -->
+		<div class="row align-items-center">
+			<div class="col-md-4">
+				<div class="row align-items-center">
+
+					<div class="col-md-4">
+						<span class="location__title">
+							Brasil
+						</span>	
+					</div>
+	
+					<div class="col-md-8 location__info_wrapper">
+						<div class="location__info">
+							<span class="location__subtitle">
+								Canela  / RS
+							</span>	
+							
+							<span class="location__text">
+								Rua Perimetral, 394<br> Distrito Industrial
+							</span>	
+	
+							<a href="tel:555432781762" class="location__phone">
+								(54) 3278 1762
+							</a>	
+
+							<a href="mailto:contato@aristeupires.com.br" class="location__text">
+								contato@aristeupires.com.br
+							</a>	
+						</div>
+
+						<div class="location__info mt-4">
+							<span class="location__subtitle">
+								Vendas Corporativas
+							</span>	
+	
+							<a href="tel:5554991581478" class="location__phone">
+								(54) 99158.1478
+							</a>	
+
+							<a href="mailto:corporativo@aristeupires.com.br" class="location__text">
+								corporativo@aristeupires.com.br
+							</a>	
+						</div>
+
+						<a href="https://www.google.com/maps/place/R.+Perimetral,+364+-+Distrito+Industrial,+Canela+-+RS,+95680-000/@-29.3394999,-50.7887841,17z/data=!3m1!4b1!4m5!3m4!1s0x95192c5fa145a291:0x89d8e2484e00b553!8m2!3d-29.3394999!4d-50.7865954" target="_blank" class="d-flex flex-row align-items-center post__card_btn mt-4"> 
+                            <span>Como chegar</span>
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                        </a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-8">
+				<img src="<?php echo $siteUrl; ?>/wp-content/uploads/2024/08/brasil.jpg" alt="foto da sede no Brasil" />
+			</div>
+
+		<!-- US LOCATION -->
+		<div class="row align-items-center flex-row-reverse mt-4">
+			<div class="col-md-4">
+				<div class="row align-items-center">
+
+					<div class="col-md-4">
+						<span class="location__title">
+							EUA
+						</span>	
+					</div>
+	
+					<div class="col-md-8 location__info_wrapper">
+						<div class="location__info">
+							<span class="location__subtitle">
+								New York / NY
+							</span>	
+							
+							<span class="location__text">
+								Sossego<br>200 Lexington<br> Ave Suite 1301
+							</span>	
+	
+							<a href="tel:2122060245" class="location__phone">
+								(212) 206.0245
+							</a>	
+						</div>
+
+						<a href="https://www.google.com/maps/place/New+York+Design+Center/@40.7454845,-73.9805782,15z/data=!4m2!3m1!1s0x0:0x7e290805cb28d637?sa=X&ved=2ahUKEwingbjG79nzAhUfpZUCHS3yBMEQ_BJ6BAh9EAU" target="_blank" class="d-flex flex-row align-items-center post__card_btn mt-4"> 
+                            <span>Como chegar</span>
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                        </a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-8">
+				<img src="<?php echo $siteUrl; ?>/wp-content/uploads/2024/08/eua.jpg" alt="foto da sede no EUA" />
+			</div>
+			
+		</div>
+	</div>
+</section>
+
 <footer>
 	<div class="container">
 		<div class="row gx-5">
-			<div class="col-md-3">
+			<div class="col-md-3 mt-5">
 				<h5 class="footer__column_title">Info</h5>
 				
 				<div class="footer__items_group">
@@ -24,7 +127,7 @@
 				</div>
 			</div>
 			
-            <div class="col-md-3">
+            <div class="col-md-3 mt-5">
 				<h5 class="footer__column_title">Produtos</h5>
 				
 				<div class="footer__items_group">
@@ -37,7 +140,7 @@
 				</div>
 			</div>
 
-            <div class="col-md-3">
+            <div class="col-md-3 mt-5">
 				<h5 class="footer__column_title">Encontre-nos</h5>
 				
 				<div class="footer__items_group">
@@ -48,7 +151,7 @@
 				</div>
 			</div>
 
-            <div class="col-md-3">
+            <div class="col-md-3 mt-5">
 				<h5 class="footer__column_title">Mantenha-se Conectado</h5>
 				
 				<div class="footer__items_group">
