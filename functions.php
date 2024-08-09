e3aa580609684928bc9c54952629714457f20527
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-08-09 14:51:35 -0300

    [FIX] Test disable include in functions

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ function oceanwp_child_enqueue_parent_style() {
 add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
 
-include("components/button-link/button-link.php");
+//include("components/button-link/button-link.php");
 include("components/post-card/post-card.php");
 include("components/product-card/product-card.php");
 
