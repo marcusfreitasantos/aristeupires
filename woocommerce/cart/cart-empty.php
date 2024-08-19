6501c7acc49377f2fcb7105a15b12bbe2a0ed707
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-08-19 11:55:20 -0300

    [FEAT] Empty cart template file.

---
+++ woocommerce/cart/cart-empty.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div  class="col-12 text-center p-5">
+				<span>Seu carrinho está vazio!</span>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
