e2fbea9739b4fd0ff12421b0140d9c30bb1afbfe
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-08-31 12:23:26 -0300

    [FEAT] Vendas corporativas page template

---
+++ page-vendas-corporativas.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section>
+    <div class="container">
+        <h1>Vendas corporativas</h1>
+        <div class="row">
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
