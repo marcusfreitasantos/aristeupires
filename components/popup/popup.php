7c9940fb533537de930f78ca36f584edda9d9ce3
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-11-18 23:34:54 -0300

    [FEAT] removed form extra button

--- components/popup/popup.php
+++ components/popup/popup.php
@@ -8,7 +8,6 @@
         $formBgColor = get_field("form_background_color", $popupObj->ID);
         $showForm = get_field("lead_form", $popupObj->ID);
         $delay = get_field("delay", $popupObj->ID);
-        $button = get_field("button",  $popupObj->ID);
 
         ?>
         <div class="custom__popup" style="background-color: <?= $bgColor; ?>">
@@ -33,10 +32,6 @@
                         <input type="submit" value="Enviar" />
                     </form>
                 <?php } ?>
-
-                <?php if($button){ 
-                    echo ButtonLink($button['url'], $button['title']);
-                } ?>
             </div>
         </div>
 
